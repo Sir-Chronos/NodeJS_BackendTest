1d9faab7b763862203a2eb3ec35536adac3b2890
Gabriel Teodoro <teodorogabriel367@gmail.com>
2024-05-01 06:02:26 -0300

    feat(DB): connection to database succesfull

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const app = express()
+//Loads all variables from the .env file and add's it to the process
+require('dotenv').config();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000 // loads the port from the .env file or use a default one
 
 app.get('/', (req, res) => {
-    res.send("Hello Nodemon!!")
+    res.send("Hello Nodemon!")
 })
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+});
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() =>{
+    console.log("Connection to database succesfull");
+})
+.catch((e) => {
+    console.log(`Connection to database failed: ${e}`);
+})
+
+
+//baPziv1MAS3Vb2i4
+//siriuswebdeveloper
